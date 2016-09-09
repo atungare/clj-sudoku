10152aa2fc215c9b5ef13ea806ed7b0a66063e82
atungare <ajay.tungare@gmail.com>
2016-09-09 19:08:04 -0400

    towards brute force solution

--- src/clj_sudoku/sudoku.clj
+++ src/clj_sudoku/sudoku.clj
@@ -1,9 +1,73 @@
 (ns clj-sudoku.sudoku)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn xy->i
+  [x y]
+    (+ x (* 9 y)))
+
+(defn rows
+  [board]
+  (for [y (range 0 9)]
+    (for [x (range 0 9)]
+      (get board (xy->i x y)))))
+
+(defn cols
+  [board]
+  (for [x (range 0 9)]
+    (for [y (range 0 9)]
+      (get board (xy->i x y)))))
+
+(defn grids
+  [board]
+  ())
+
+(defn board->sections
+  [board]
+  (mapcat #(% board) [rows cols grids]))
+
+(defn isComplete?
+  [section_or_board]
+  (not-any? zero? section_or_board))
+
+(defn isSectionValid?
+  [section]
+  (apply distinct? (remove zero? section)))
+
+(defn isSectionSolved?
+  [section]
+  (and (isComplete? section) (isSectionValid? section)))
+
+(defn isBoardSolved?
+  [board]
+  (every? isSectionSolved? (board->sections board)))
+
+(defn updateFirst
+  [x y lst]
+  (:lst
+    (reduce
+      (fn [acc item]
+        (if (and (not (:seen acc)) (= item x))
+          {:lst (cons y acc) :seen true}
+          {:lst (cons item acc) :seen (:seen acc)}))
+      {:lst [] :seen false}
+      lst)))
+
+(defn updateBoard
+  [board candidate]
+  (updateFirst 0 candidate board))
+
+(defn solveBoard
+  [board]
+  (if (isComplete? board)
+    (if (isBoardSolved? board)
+      board
+      nil)
+    (loop [candidate 1]
+      (if (> candidate 9)
+        nil
+        (let [solution (solveBoard (updateBoard board candidate))]
+          (if solution
+            solution
+            (recur (inc candidate))))))))
 
 (defn -main [& args]
- (foo "jill"))
+ (println "Hello"))
--- test/clj_sudoku/sudoku_test.clj
+++ test/clj_sudoku/sudoku_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [clj-sudoku.sudoku :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def board [0 0 3  0 2 0  6 0 0
+            9 0 0  3 0 5  0 0 1
+            0 0 1  8 0 6  4 0 0
+
+            0 0 8  1 0 2  9 0 0
+            7 0 0  0 0 0  0 0 8
+            0 0 6  7 0 8  2 0 0
+
+            0 0 2  6 0 9  5 0 0
+            8 0 0  2 0 3  0 0 9
+            0 0 5  0 1 0  3 0 0])
+
+
+(def row-soln '((0 0 3 0 2 0 6 0 0) (9 0 0 3 0 5 0 0 1) (0 0 1 8 0 6 4 0 0) (0 0 8 1 0 2 9 0 0) (7 0 0 0 0 0 0 0 8) (0 0 6 7 0 8 2 0 0) (0 0 2 6 0 9 5 0 0) (8 0 0 2 0 3 0 0 9) (0 0 5 0 1 0 3 0 0)))
+(deftest rows-test
+  (testing "rows"
+    (is (= (rows board) row-soln))))
+
+(def col-soln '((0 9 0 0 7 0 0 8 0) (0 0 0 0 0 0 0 0 0) (3 0 1 8 0 6 2 0 5) (0 3 8 1 0 7 6 2 0) (2 0 0 0 0 0 0 0 1) (0 5 6 2 0 8 9 3 0) (6 0 4 9 0 2 5 0 3) (0 0 0 0 0 0 0 0 0) (0 1 0 0 8 0 0 9 0)))
+(deftest cols-test
+  (testing "cols"
+    (is (= (cols board) col-soln))))
